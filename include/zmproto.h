9408dffdd262477edfacf2c162db1b2dff0d486b
Tomas Halman <tomas@halman.net>
2017-05-29 07:05:06 +0200

    Problem: No code in project
    Solution: Generate from xml
    
    Signed-off-by: Tomas Halman <Tomas@Halman.net>

---
+++ include/zmproto.h
@@ -0,0 +1,21 @@
+/*  =========================================================================
+    zm-proto - Basic messaging for zmon.it
+
+    Copyright (c) the Contributors as noted in the AUTHORS file.  This file is part
+    of zmon.it, the fast and scalable monitoring system.                           
+                                                                                   
+    This Source Code Form is subject to the terms of the Mozilla Public License, v.
+    2.0. If a copy of the MPL was not distributed with this file, You can obtain   
+    one at http://mozilla.org/MPL/2.0/.                                            
+    =========================================================================
+*/
+
+#ifndef ZMPROTO_H_H_INCLUDED
+#define ZMPROTO_H_H_INCLUDED
+
+//  Include the project library file
+#include "zm_proto_library.h"
+
+//  Add your own public definitions here, if you need them
+
+#endif

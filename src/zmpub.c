41fb5bc3cc691e7c41c0c55f78cb345803a2e89b
Michal Vyskocil <michalvyskocil@eaton.com>
2017-05-29 08:17:45 +0200

    Problem: need command line tools for messages
    Solution: add zmpub command

---
+++ src/zmpub.c
@@ -0,0 +1,47 @@
+/*  =========================================================================
+    zmpub - Helper tool to publish zm-proto messages on a stream
+
+    Copyright (c) the Contributors as noted in the AUTHORS file.  This file is part
+    of zmon.it, the fast and scalable monitoring system.                           
+                                                                                   
+    This Source Code Form is subject to the terms of the Mozilla Public License, v.
+    2.0. If a copy of the MPL was not distributed with this file, You can obtain   
+    one at http://mozilla.org/MPL/2.0/.                                            
+    =========================================================================
+*/
+
+/*
+@header
+    zmpub - Helper tool to publish zm-proto messages on a stream
+@discuss
+@end
+*/
+
+#include "zm_proto_classes.h"
+
+int main (int argc, char *argv [])
+{
+    bool verbose = false;
+    int argn;
+    for (argn = 1; argn < argc; argn++) {
+        if (streq (argv [argn], "--help")
+        ||  streq (argv [argn], "-h")) {
+            puts ("zmpub [options] ...");
+            puts ("  --verbose / -v         verbose test output");
+            puts ("  --help / -h            this information");
+            return 0;
+        }
+        else
+        if (streq (argv [argn], "--verbose")
+        ||  streq (argv [argn], "-v"))
+            verbose = true;
+        else {
+            printf ("Unknown option: %s\n", argv [argn]);
+            return 1;
+        }
+    }
+    //  Insert main code here
+    if (verbose)
+        zsys_info ("zmpub - Helper tool to publish zm-proto messages on a stream");
+    return 0;
+}
